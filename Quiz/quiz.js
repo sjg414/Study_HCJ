ac8fde4c01f462db6283868f259d99a9e2ce07a1
sjg414 <sjg414@naver.com>
2024-06-13 20:37:20 +0900

    2024.06.13 Quiz
    css 수정
    이전, 다음 버튼 추가

--- Quiz/quiz.js
+++ Quiz/quiz.js
@@ -11,33 +11,110 @@ var quizText = [
     answer: 1,
   },
 ];
-var count = 0;
+var count = 1;
 
 //rendering
 window.onload = quizLoad();
 
 //답안 클릭 시
 function answerClick(clickValue) {
-  if (quizText[count].answer === clickValue) {
+  if (quizText[count - 1].answer === clickValue) {
     alert("정답입니다.");
-    count++;
-    var answer = document.querySelectorAll("#answer");
-    answer[0].innerHTML = "";
+    ++count;
     quizLoad();
   } else alert("틀렸습니다.");
 }
 
 //quiz load
 function quizLoad() {
-  //문제 불러오기
   var question = document.getElementById("questionText");
-  var questText = document.createTextNode(quizText[count].questionText);
+  var answer = document.querySelectorAll("#answer");
+
+  //문제가 끝났을 경우 종료
+  if (count - 1 === quizText.length) {
+    alert("문제가 끝났습니다.");
+    return 0;
+  }
+
+  //새로운 문제 load 시 기존 문제 삭제
+  if (count - 1 > 0) {
+    question.textContent = "";
+    for (let i = 0; i < answer.length; i++) answer[i].textContent = "";
+  }
+
+  //문제 불러오기
+  var questText = document.createTextNode(
+    count + "번 " + quizText[count - 1].questionText
+  );
   question.appendChild(questText);
 
   //선택답안 불러오기
-  var answer = document.querySelectorAll("#answer");
   for (let i = 0; i < answer.length; i++) {
-    let answerText = document.createTextNode(quizText[count].answerText[i]);
+    let answerText = document.createTextNode(quizText[count - 1].answerText[i]);
     answer[i].appendChild(answerText);
+  }
+}
+
+//이전버튼 클릭 시
+function preButton() {
+  var question = document.getElementById("questionText");
+  var answer = document.querySelectorAll("#answer");
+  count -= 1;
+
+  //첫번째 문제일 경우
+  if (count < 1) {
+    alert("1번 문제입니다.");
+    count = 1;
+    return 0;
+  } else {
+    //새로운 문제 load 시 기존 문제 삭제
+    question.textContent = "";
+    for (let i = 0; i < answer.length; i++) answer[i].textContent = "";
+
+    //문제 불러오기
+    var questText = document.createTextNode(
+      count + "번 " + quizText[count - 1].questionText
+    );
+    question.appendChild(questText);
+
+    //선택답안 불러오기
+    for (let i = 0; i < answer.length; i++) {
+      let answerText = document.createTextNode(
+        quizText[count - 1].answerText[i]
+      );
+      answer[i].appendChild(answerText);
+    }
+  }
+}
+
+//다음버튼 클릭 시
+function nextButton() {
+  var question = document.getElementById("questionText");
+  var answer = document.querySelectorAll("#answer");
+  count += 1;
+
+  //마지막 문제일 경우
+  if (count - 1 === quizText.length) {
+    alert("문제가 끝났습니다.");
+    count = quizText.length;
+    return 0;
+  } else {
+    //새로운 문제 load 시 기존 문제 삭제
+    question.textContent = "";
+    for (let i = 0; i < answer.length; i++) answer[i].textContent = "";
+
+    //문제 불러오기
+    var questText = document.createTextNode(
+      count + "번 " + quizText[count - 1].questionText
+    );
+    question.appendChild(questText);
+
+    //선택답안 불러오기
+    for (let i = 0; i < answer.length; i++) {
+      let answerText = document.createTextNode(
+        quizText[count - 1].answerText[i]
+      );
+      answer[i].appendChild(answerText);
+    }
   }
 }
